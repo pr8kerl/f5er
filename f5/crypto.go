16c5b216b2ea258be02cf848644c42c615988a09
Trevor Linton <trevor.linton@gmail.com>
2017-07-31 13:06:21 -0600

    Fix to add serverName into LBClientSSL

--- f5/crypto.go
+++ f5/crypto.go
@@ -33,7 +33,7 @@ type SSLCertificate struct {
 	SerialNum	string 		`json:"serialNumber"`
 	Size 		int			`json:"size"`
 	Subject		string 		`json:"subject"`
-	UpdatedBy	string 		`json:"updatedBy"`
+	UpdatedBy	string		`json:"updatedBy"`
 	Version		int			`json:"version"`
 }
 
